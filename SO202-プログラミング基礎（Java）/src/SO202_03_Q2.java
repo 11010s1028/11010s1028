754956bac8d842689da98e27e6a30a8bc2836fd0
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-03 13:06:10 +0900

    Initial commit

---
+++ SO202-プログラミング基礎（Java）/src/SO202_03_Q2.java
@@ -0,0 +1,25 @@
+/**
+プログラミング基礎Java
+変数とデータ型
+練習問題2
+以下の文字列を変数に代入して表示してください。
+    He said "Hello World!!".
+
+コンパイル方法
+================
+>>> java -jar checkstyle-10.3.4-all.jar -c checkstyle.xml SO202_03_Q2.java
+Starting audit...
+Audit done.
+>>> javac SO202_03_Q2.java
+================
+
+実行例
+================
+>>> java SO202_03_Q2
+He said "Hello World!!".
+================
+
+*/
+
+
+// ここより下に解答を記載する。
---
+++ SO202-プログラミング基礎（Java）/src/SO202_04_Q3.java
@@ -0,0 +1,25 @@
+/**
+プログラミング基礎Java
+演算子
+練習問題3
+以下の文字列を表示してください。
+    2つの文字列「Good」と「Morning」を結合した文字列
+
+コンパイル方法
+================
+>>> java -jar checkstyle-10.3.4-all.jar -c checkstyle.xml SO202_04_Q3.java
+Starting audit...
+Audit done.
+>>> javac SO202_04_Q3.java
+================
+
+実行例
+================
+>>> java SO202_04_Q3
+GoodMorning
+================
+
+*/
+
+
+// ここより下に解答を記載する。
