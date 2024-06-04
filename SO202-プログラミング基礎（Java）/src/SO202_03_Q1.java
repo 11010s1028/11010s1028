f7224d94e6af4a9a33a710ac001ce9c766345676
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-04 14:40:53 +0900

    Update SO202_03_Q1.java

--- SO202-プログラミング基礎（Java）/src/SO202_03_Q1.java
+++ SO202-プログラミング基礎（Java）/src/SO202_03_Q1.java
@@ -35,7 +35,6 @@ public class SO202_03_Q1 {
   public static void main(String[] args) {
     String bin = "1000";
     String hex = "1000";
-    double c = 0.56;
     double d = 7.8;
     int i = Integer.parseInt(bin, 2); // 2 → 10変換
     int j = Integer.parseInt(hex, 16); // 16 → 10変換
@@ -45,8 +44,9 @@ public class SO202_03_Q1 {
     d = d * result;
     System.out.printf("%d\n", i);
     System.out.printf("%d\n", j);
-    System.out.printf("%.2f\n", c);
-    System.out.println(d);
+      double c = 0.56;
+      System.out.printf("%.2f\n", c);
+      System.out.println(d);
   }
 }
 
