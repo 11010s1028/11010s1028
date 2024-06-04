da7cf04b04ade40c02ad79c47c572ae13065789c
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-04 11:15:21 +0900

    Update SO202_03_Q1.java

--- SO202-プログラミング基礎（Java）/src/SO202_03_Q1.java
+++ SO202-プログラミング基礎（Java）/src/SO202_03_Q1.java
@@ -51,7 +51,7 @@ public class SO202_03_Q1 {
         System.out.printf("%d\n", i);
         System.out.printf("%d\n", j);
         System.out.printf("%.2f\n", c);
-        System.out.println(d);
+        System.out.println(d\n);
     }
 }
 
