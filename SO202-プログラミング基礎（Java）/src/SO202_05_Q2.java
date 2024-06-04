9515f2f024c5f98960a0d352267da11264907381
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-04 16:40:18 +0900

    Update SO202_05_Q2.java

--- SO202-プログラミング基礎（Java）/src/SO202_05_Q2.java
+++ SO202-プログラミング基礎（Java）/src/SO202_05_Q2.java
@@ -23,3 +23,26 @@ numを3で割った余りは、2です。
 
 
 // ここより下に解答を記載する。
+public class SO202_05_Q2 {
+  public static void main(String[] args) {
+    
+    int num = 8;
+
+    switch (num % 3) {
+      case 0:
+        System.out.println("numを3で割った余りは、0です。");
+        break;
+      case 1:
+        System.out.println("numを3で割った余りは、1です。");
+        break;
+      case 2:
+        System.out.println("numを3で割った余りは、2です。");
+        break;
+      default:
+        System.out.println("numに誤りがあります、");
+        break;
+    }
+  }
+}
+
+  
