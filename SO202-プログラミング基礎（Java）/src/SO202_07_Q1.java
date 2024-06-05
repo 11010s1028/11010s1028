e63ed0739cb5deabc79a99cc997c13c8c4c1a1fa
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-05 11:35:15 +0900

    Update SO202_07_Q1.java

--- SO202-プログラミング基礎（Java）/src/SO202_07_Q1.java
+++ SO202-プログラミング基礎（Java）/src/SO202_07_Q1.java
@@ -41,34 +41,6 @@ B高校のテニス部に入っています。
 
 
 // ここより下に解答を記載する。
-class Student {
-  static String school = "A高校";
-
-  String name;
-  String club;
-  String subject;
-
-  Student(String name, String club, String subject) {
-
-    this.name = name;
-    this.club = club;
-    this.subject = subject;
-
-  }
-
-　　String introduce() {
-    
-    String text = String.format("私の名前は%sです。\n", this.name);
-
-    text += String.format("%sの%sに入っています。\n", school, this.club);
-
-    text += String.format("私の好きな教科は%sです。\n", this.subject);
-
-    return text;
-  }
-}
-
-
 public class SO202_07_Q1 {
   public static void main(String[] args) {
 
