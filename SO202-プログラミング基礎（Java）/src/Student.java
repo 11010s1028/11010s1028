6f5b150a91ef8e1ac0ffb2a27138d20ab350d967
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-05 11:35:43 +0900

    Update Student.java

--- SO202-プログラミング基礎（Java）/src/Student.java
+++ SO202-プログラミング基礎（Java）/src/Student.java
@@ -1,1 +1,27 @@
 // ここより下に解答を記載する。
+class Student {
+  static String school = "A高校";
+
+  String name;
+  String club;
+  String subject;
+
+  Student(String name, String club, String subject) {
+
+    this.name = name;
+    this.club = club;
+    this.subject = subject;
+
+  }
+
+　　String introduce() {
+    
+    String text = String.format("私の名前は%sです。\n", this.name);
+
+    text += String.format("%sの%sに入っています。\n", school, this.club);
+
+    text += String.format("私の好きな教科は%sです。\n", this.subject);
+
+    return text;
+  }
+}
