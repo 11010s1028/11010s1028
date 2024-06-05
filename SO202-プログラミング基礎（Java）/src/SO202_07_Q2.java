025c90cc9b4bf614d588f62e0f6aec14fc8ce2d3
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-05 14:30:43 +0900

    Update SO202_07_Q2.java

--- SO202-プログラミング基礎（Java）/src/SO202_07_Q2.java
+++ SO202-プログラミング基礎（Java）/src/SO202_07_Q2.java
@@ -31,7 +31,7 @@ public class SO202_07_Q2 {
 
     LocalDateTime date = LocalDateTime.of(2000, 1, 2, 3, 4, 5);
 
-    DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒");
     System.out.println(date.format(f));
 
   }
