417c405f6b77587720ff0380c169485954e3c924
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-06 13:42:48 +0900

    Update SO202_09_Q3.java

--- SO202-プログラミング基礎（Java）/src/SO202_09_Q3.java
+++ SO202-プログラミング基礎（Java）/src/SO202_09_Q3.java
@@ -23,3 +23,42 @@ Audit done.
 
 
 // ここより下に解答を記載する。
+public class SO202_09_Q3 {
+  public static void main(String[] args) {
+
+    int[] data = {2, 13, 99, 20, 28, 64, 33, 73, 20, 1, 73};
+
+    int n = data.length;
+
+ 
+    for (int i = 0; i < n - 1; i++) {
+
+      int minIndex = i;
+
+      for (int j = i + 1; j < n; j++) {
+        
+        if (data[j] < data[minIndex]) {
+          minIndex = j;
+          
+        }
+      }
+
+      int min = data[minIndex];
+      data[minIndex] = data[i];
+      data[i] = min;
+    
+    }
+
+    for (int i = 0; i < n; i++){
+
+      if (i == 10) {
+        System.out.printf("%d", data[i]);
+      } else {
+        System.out.printf("%d, ", data[i]);
+      
+      }
+    }
+  
+  }
+
+}
