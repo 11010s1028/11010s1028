986608d6002a7d105f662e541ebf9afdb5645e09
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-13 13:20:13 +0900

    Update SO302_11_Q1_3.java

--- SO302-プログラミング実践（Java）/src/SO302_11_Q1_3.java
+++ SO302-プログラミング実践（Java）/src/SO302_11_Q1_3.java
@@ -42,8 +42,62 @@ B高校の吹奏楽部に入っています。
 
 */
 
-public class SO302_11_Q1_3 {
+public class SO302_11_Q1_2 {
   public static void main(String[] args) {
     
-  }
+    // 高校の情報
+    String koukou1 = "A高校";
+
+    // 学生の情報
+    
+
+    String name1 = "山田";
+    String name2 = "田中";
+    String name3 = "鈴木";
+
+    String bukatu1 = "野球部";
+    String bukatu2 = "テニス部";
+    String bukatu3 = "吹奏楽部";
+
+    String kyouka1 = "国語";
+    String kyouka2 = "数学";
+    String kyouka3 = "社会";
+      
+    
+
+    // 自己紹介  
+
+    System.out.println ("私の名前は" + name1 + "です。");
+    System.out.println (koukou1 + "の" + bukatu1 + "に入っています。");
+    System.out.println ("私の好きな教科は" + kyouka1 + "です。\n");
+
+    System.out.println ("私の名前は" + name2 + "です。");
+    System.out.println (koukou1 + "の" + bukatu2 + "に入っています。");
+    System.out.println ("私の好きな教科は" + kyouka2 + "です。\n");
+
+    System.out.println ("私の名前は" + name3 + "です。");
+    System.out.println (koukou1 + "の" + bukatu3 + "に入っています。");
+    System.out.println ("私の好きな教科は" + kyouka3 + "です。\n");
+
+
+
+    // 高校の情報を再設定
+    String koukou2 = "B高校";
+
+    // 自己紹介
+    
+
+    System.out.println ("私の名前は" + name1 + "です。");
+    System.out.println (koukou2 + "の" + bukatu1 + "に入っています。");
+    System.out.println ("私の好きな教科は" + kyouka1 + "です。\n");
+
+    System.out.println ("私の名前は" + name2 + "です。");
+    System.out.println (koukou2 + "の" + bukatu2 + "に入っています。");
+    System.out.println ("私の好きな教科は" + kyouka2 + "です。\n");
+
+    System.out.println ("私の名前は" + name3 + "です。");
+    System.out.println (koukou2 + "の" + bukatu3 + "に入っています。");
+    System.out.println ("私の好きな教科は" + kyouka3 + "です。\n");
+   }
+  
 }
