8861bc97214c361177d74fa560762c7e480d10d6
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-13 12:58:37 +0900

    Update SO302_11_Q1_1.java

--- SO302-プログラミング実践（Java）/src/SO302_11_Q1_1.java
+++ SO302-プログラミング実践（Java）/src/SO302_11_Q1_1.java
@@ -35,75 +35,45 @@ B高校のテニス部に入っています。
 */
 
 public class SO302_11_Q1_1 {
+  public static void main(String[] args) {
     
     // 高校の情報
-    public class KoukouClass {
-      public String KoukouMethod() {
-
-        return "A高校";
-        
-      }
-    }
+    public String koukou1 = "A高校";
 
     // 学生の情報
-    public class GakuseiClass {
+    
 
-      public String name1 = "山田";
-      public String name2 = "田中";
+    public String name1 = "山田";
+    public String name2 = "田中";
 
-      public String bukatu1 = "野球部";
-      public String bukatu2 = "テニス部";
+    public String bukatu1 = "野球部";
+    public String bukatu2 = "テニス部";
       
-    }
+    
 
     // 自己紹介
-    public class JikoClass {
+   
 
-      GakuseiClass gakusei = new GakuseiClass();
-      KoukouClass koukou = new KoukouClass();
-      
-      public String JikoMethod {
+    GakuseiClass gakusei = new GakuseiClass();    
 
-        System.out.println("私の名前は" + gakusei.name1 + "です。");
-        System.out.println(koukou.KoukouMethod + "の" + gakusei.bukatu1 + "に入っています。");
+    System.out.println("私の名前は" + name1 + "です。");
+    System.out.println(koukou1 + "の" + gakusei.bukatu1 + "に入っています。");
 
-        System.out.println("私の名前は" + gakusei.name2 + "です。");
-        System.out.println(koukou.KoukouMethod + "の" + gakusei.bukatu2 + "に入っています。");
+    System.out.println("私の名前は" + name2 + "です。");
+    System.out.println(koukou1 + "の" + bukatu2 + "に入っています。");
 
-      }
-      
-    }
 
     // 高校の情報を再設定
-    public class Koukou2Class {
-      public String Koukou2Method {
-
-        return "B高校";
-      }
-    }
+    public String koukou2 = "B高校";
 
     // 自己紹介
-    public class Jiko2Class {
-      
-      //GakuseiClass gakusei = new GakuseiClass();
-      Koukou2Class koukou2 = new Koukou2Class();
-      
-      public String JikoMethod {
+    
 
-        System.out.println("私の名前は" + gakusei.name1 + "です。");
-        System.out.println(koukou2.Koukou2Method + "の" + gakusei.bukatu1 + "に入っています。");
+    System.out.println("私の名前は" + name1 + "です。");
+    System.out.println(koukou2 + "の" + bukatu1 + "に入っています。");
 
-        System.out.println("私の名前は" + gakusei.name2 + "です。");
-        System.out.println(koukou2.Koukou2Method + "の" + gakusei.bukatu2 + "に入っています。");
-
-      }
-      
-    }
-
-   public static void main(String[] args) {
-
-     JikoClass jiko = new jikoClass();
-     Jiko2Class jiko2 = new Jiko2Class();
+    System.out.println("私の名前は" + name2 + "です。");
+    System.out.println(koukou2 + "の" + bukatu2 + "に入っています。");
    }
   
 }
