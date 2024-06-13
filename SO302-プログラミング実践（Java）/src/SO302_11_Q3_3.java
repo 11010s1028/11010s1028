aa998208ce018f9863c24b49fbcdd132f291d8e8
11010s1028 <h-hiratsuka@lis.co.jp>
2024-06-13 14:29:30 +0900

    Update SO302_11_Q3_3.java

--- SO302-プログラミング実践（Java）/src/SO302_11_Q3_3.java
+++ SO302-プログラミング実践（Java）/src/SO302_11_Q3_3.java
@@ -44,8 +44,82 @@ B高校の吹奏楽部に入っています。
 
 public class SO302_11_Q3_3 {
   public static void main(String[] args) {
+    
+   /* // 高校の情報
+    String koukou1 = "A高校";
 
-  }
+    // 高校の情報を再設定
+    String koukou2 = "B高校";
+
+    // 学生の情報
+    
+
+    String name1 = "山田";
+    String name2 = "田中";
+
+    String bukatu1 = "野球部";
+    String bukatu2 = "テニス部"; 
+
+    //SO302_11_Q2_1 SO = new SO302_11_Q2_1(); //技：staticじゃないメソッドを使うため
+
+
+    jiko(name1, koukou1, bukatu1);
+
+    jiko(name2, koukou1, bukatu2);
+
+    jiko(name1, koukou2, bukatu1);
+
+    jiko(name2, koukou2, bukatu2);
+      
+    
+
+    // 自己紹介  
+
+    System.out.println ("私の名前は" + name1 + "です。");
+    System.out.println (koukou1 + "の" + bukatu1 + "に入っています。\n");
+
+    System.out.println ("私の名前は" + name2 + "です。");
+    System.out.println (koukou1 + "の" + bukatu2 + "に入っています。\n");
+
+
+    
+
+    // 自己紹介
+    
+
+    System.out.println ("私の名前は" + name1 + "です。");
+    System.out.println (koukou2 + "の" + bukatu1 + "に入っています。\n");
+
+    System.out.println ("私の名前は" + name2 + "です。");
+    System.out.println (koukou2 + "の" + bukatu2 + "に入っています。\n");
+    */
+
+    String[] koukou = {"A高校", "B高校"};
+
+    String[] name = {"山田", "田中", "鈴木"};
+
+    String[] bukatu = {"野球部", "テニス部", "吹奏楽部"};
+
+    String[] kyouka = {"国語", "数学", "社会"};
+
+    for (int i = 0; i < 3; i++){
+
+      System.out.println ("私の名前は" + name[i] + "です。");
+      System.out.println (koukou[0] + "の" + bukatu[i] + "に入っています。");
+      System.out.println ("私の好きな教科は" + kyouka[i] + "です。\n");
+    
+    }
+
+    for (int i = 0; i < 3; i++){
+
+      System.out.println ("私の名前は" + name[i] + "です。");
+      System.out.println (koukou[1] + "の" + bukatu[i] + "に入っています。");
+      System.out.println ("私の好きな教科は" + kyouka[i] + "です。\n");
+    
+    }
   
-  // 関数の定義
+  }
+
+  
 }
+
